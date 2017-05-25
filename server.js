3e57542dbcd0a109d60394b3a5a57f98da05fb46
Peter Markoski <pmarko.alt@gmail.com>
2017-05-25 19:23:47 -0400

    functioning sign up

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require("express");
 var methodOverride = require("method-override");
 var bodyParser = require("body-parser");
+var path = require("path");
 var app = express();
 var db = require("./models");
+
+
 
 
 app.set('port', (process.env.PORT || 5000));
@@ -13,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // Override with POST having ?_method=DELETE
 app.use(methodOverride("_method"));
-
+//
 // Set Handlebars.
 var exphbs = require("express-handlebars");
 
@@ -25,8 +28,32 @@ app.set("view engine", "handlebars");
 
 // app.use("/", routes);
 
+
 db.sequelize.sync().then(function() {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 });
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "./login.html"));
+});
+
+app.post("/", function(req, res) {
+  db.client.create({
+    firstName: req.body.first_name,
+    lastName: req.body.last_name,
+    userName: req.body.userName,
+    email: req.body.email,
+    password: req.body.password,
+    streetAddress: req.body.street_address,
+    city: req.body.city,
+    state: req.body.state,
+    zipcode: req.body.zipcode
+  }).then(function(data){
+
+  }).catch(function(err){
+    console.log(err);
+  });
+  console.log(req.body);
+})
